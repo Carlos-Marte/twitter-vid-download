d7be304168812137361e5da9f17b13cf0f3f04fd
CarlosMarte <carlosmartedev@hotmail.com>
2024-02-26 17:45:20 -0400

    feat: :sparkles: añadiendo el formulario a la página Home

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { routes } from '@/routes/routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,6 +1,61 @@
+import Stack from 'react-bootstrap/Stack';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { urlRegExp } from '@/utils/regexps/regexps';
+
 const Home = () => {
+  const [inputURL, setInputURL] = useState('');
+  const [inputError, setInputError] = useState(null);
+
+  const handleChangeInput = ({ target: { value } }) => {
+    setInputURL(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (inputURL.length === 0) {
+      return setInputError(true);
+    }
+
+    if (!urlRegExp.test(inputURL)) {
+      return setInputError(true);
+    }
+
+    setInputError(null);
+    setInputURL('');
+  };
+
   return (
-    <div>Home</div>
+    <Stack gap={3} className='pt-5 mx-auto' style={{ maxWidth: '700px' }}>
+      <h1 className='fs-1 text-center'>TwitterVidDownload</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId='formTwitterUrl' className='mb-3 mx-3'>
+          {inputError
+            ? (
+            <Form.Control
+              placeholder='Insert link'
+              type='text' value={inputURL}
+              onChange={handleChangeInput}
+              isInvalid
+            >
+            </Form.Control>
+              )
+            : (
+            <Form.Control
+              placeholder='Insert link'
+              type='text' value={inputURL}
+              onChange={handleChangeInput}
+            >
+            </Form.Control>
+              )}
+        </Form.Group>
+
+        <Button className='mx-auto d-block' type='submit'>Download</Button>
+      </Form>
+    </Stack>
   );
 };
 export default Home;
